af78d8d6dbe23b495d4666d24e22fbcffa9fc190
Dewtime5519 <pokamondew@gmail.com>
2025-05-06 22:59:46 +0700

    อันนี้เสร็จจริงๆละ

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,15 +15,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private CircleCollider2D cc;
 
-    private float cicumference;
+    private float circumference;
     private bool isGrounded = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         cc = GetComponent<CircleCollider2D>();
-        cicumference = 2 * Mathf.PI * cc.radius;
+        circumference = 2 * Mathf.PI * cc.radius;
     }
-    
+
     void Update()
     {
         isGrounded = cc.IsTouchingLayers(groundLayer);
@@ -38,10 +39,10 @@ public class PlayerController : MonoBehaviour
 
     void AnimationUpdate()
     {
-        float speed = rb.velocity.magnitude;
+        float speed = rb.linearVelocity.magnitude;
         if (speed > 0)
         {
-            transform.Rotate(0,0, (speed * -Mathf.Sign(rb.velocity.x) * Time.deltaTime / cicumference) * 360f * rollDegreeMultiplier);
+            transform.Rotate(0, 0, (speed * -Mathf.Sign(rb.linearVelocity.x) * Time.deltaTime / circumference) * 360f * rollDegreeMultiplier);
         }
     }
 
@@ -49,28 +50,24 @@ public class PlayerController : MonoBehaviour
     {
         if (h != 0)
         {
-            rb.velocity += h * accelerationSpeed * Vector2.right * Time.deltaTime;
-            if (rb.velocity.x > maxRollSpeed || rb.velocity.x < -maxRollSpeed)
+            rb.linearVelocity += h * accelerationSpeed * Vector2.right * Time.deltaTime;
+            if (rb.linearVelocity.x > maxRollSpeed || rb.linearVelocity.x < -maxRollSpeed)
             {
-                rb.velocity = new Vector2(h * maxRollSpeed, rb.velocity.y);
+                rb.linearVelocity = new Vector2(h * maxRollSpeed, rb.linearVelocity.y);
             }
         }
-        else if (rb.velocity.x != 0)
+        else if (rb.linearVelocity.x != 0)
         {
-            float deceleratedSpeed = Mathf.Lerp(rb.velocity.x, 0, decelerationSpeed * Time.deltaTime);
-            rb.velocity = new Vector2(deceleratedSpeed, rb.velocity.y);
+            float deceleratedSpeed = Mathf.Lerp(rb.linearVelocity.x, 0, decelerationSpeed * Time.deltaTime);
+            rb.linearVelocity = new Vector2(deceleratedSpeed, rb.linearVelocity.y);
         }
     }
 
     void Jump()
     {
-        if (!isGrounded) 
+        if (isGrounded)
         {
-            List<ContactPoint2D> contacts = new List<ContactPoint2D>();
-            cc.GetContacts(contacts);
-            rb.velocity += contacts[0].normal * jumpSpeed;
-            
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
         }
     }
-
 }
